c08e898154213bf6afcce07525c9e01fbe703e30
athararyasatya <133422563+athararyasatya@users.noreply.github.com>
2024-02-09 21:15:01 +0700

    tes commit 1

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 //toggle class active
 const headerExtra = document.querySelector('.header-extra');
+headerExtra.classList.remove('active');
+
 //Pada saat buregr menu di click
-document.querySelector('#hamburger').onclick = () =>{headerExtra.classList.toggle('active')}; 
+//di tambahin event event) => {
+    // event.preventDefault() biar ga ke atas pas di klik hamburgeernya
+document.querySelector('#hamburger').onclick = (event) => {
+    event.preventDefault(); // Mencegah perilaku default dari link
+    headerExtra.classList.toggle('active');
+}; 
+
 //clik di luar sidbar biar ga muncul side bar
-const ham = document.querySelector ('hamburger');
+const burger = document.querySelector ('hamburger');
 
 document.addEventListener('click',function(e) {
+    
     if(!hamburger.contains(e.target) &&!headerExtra.contains(e.target)) {
         headerExtra.classList.remove('active');
     }
     
 });
+
+
